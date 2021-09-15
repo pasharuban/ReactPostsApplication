2f02cfba822d35ae4597cbe6237673f9695b7e2c
Pavel Ruban <pavelruban@bk.ru>
2021-09-15 13:03:07 +0300

    created app.js component with test code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/app"; //вебпак автоматически найдет index.js внутри app
 
 ReactDOM.render(
   <React.StrictMode>
