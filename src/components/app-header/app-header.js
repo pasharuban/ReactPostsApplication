94bf6f031358145113e3dfb6e3fe63b7588167a5
Pavel Ruban <pavelruban@bk.ru>
2021-09-15 16:31:35 +0300

    initialized all components with template's code

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Paul Ruban</h1>
+      <h2>5 записей, из них понравилось:0</h2>
+    </div>
+  );
+};
+export default AppHeader;
